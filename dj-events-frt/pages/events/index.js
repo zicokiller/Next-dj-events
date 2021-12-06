f6fa599f48f15da78c3697dbd081602e75ea6b03
zicokiller <mrichet.cb@gmail.com>
2021-12-06 21:27:00 +0100

    feature: create head layout component and wrap all my pages

--- dj-events-frt/pages/events/index.js
+++ dj-events-frt/pages/events/index.js
@@ -1,7 +1,9 @@
+import Layout from "../../components/Layout";
+
 export default function EventsPage() {
   return (
-    <div>
+    <Layout>
       <h1>My events</h1>
-    </div>
+    </Layout>
   )
 }
