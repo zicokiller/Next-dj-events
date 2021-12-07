16ce5fa159cd37205848f125c4b925401d5c12cc
zicokiller <mrichet.cb@gmail.com>
2021-12-07 19:56:44 +0100

    Feature: Create Showcase component (hero banner)

--- dj-events-frt/components/Layout.js
+++ dj-events-frt/components/Layout.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import styles from "@/styles/Layout.module.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Showcase from "./Showcase";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -12,9 +15,8 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
-      <div className={styles.container}>
-        {children}
-      </div>
+      {router.pathname === "/" && <Showcase />}
+      <div className={styles.container}>{children}</div>
       <Footer />
     </div>
   );
