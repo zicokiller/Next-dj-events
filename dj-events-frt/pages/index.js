d8becc0253ff287fd09d4ee707d57dbcef23de3e
zicokiller <mrichet.cb@gmail.com>
2021-12-10 17:49:06 +0100

    Feature: Create EventItem, add and map it to homepage

--- dj-events-frt/pages/index.js
+++ dj-events-frt/pages/index.js
@@ -1,4 +1,6 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import Link from "next/link";
 import { API_URL } from "@/config/index";
 
 export default function HomePage({events}) {
@@ -6,18 +8,27 @@ export default function HomePage({events}) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 &&(
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
 
   //console.log(events); // display les data ds le terminal (server)
 
   return {
-    props: {events},
+    props: {events: events.slice(0,3)},
     revalidate: 1
   };
 }
