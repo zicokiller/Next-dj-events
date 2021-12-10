f4f322e4899c48f28f7d4c6da561fda3f126e23d
zicokiller <mrichet.cb@gmail.com>
2021-12-10 16:47:31 +0100

    Feature: Create jsconfig.json to use shorthand with '@/' and config folder with index file to fetch events data with getServerSideProps() in Home

--- dj-events-frt/pages/index.js
+++ dj-events-frt/pages/index.js
@@ -1,9 +1,23 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({events}) {
+  //console.log(events); //display les data ds le browser (client)
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  //console.log(events); // display les data ds le terminal (server)
+
+  return {
+    props: {events},
+    revalidate: 1
+  };
 }
